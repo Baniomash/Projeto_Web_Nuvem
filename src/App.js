cf80782c47174c5ab1b1578be6bd2d937789d28c
Baniomash <jonatase123.je@gmail.com>
2022-12-01 09:55:52 -0300

    feat: creating the main screen; fix: docker-compose settings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+
+const busca = (tipo) => {
+  if (tipo === 'Todos') {
+    alert('opa!');
+  } else if (tipo === 'Estaduais') {
+
+  } else if (tipo === 'Nacionais') {
+
+  } else if (tipo === 'Internacionais') {
+
+  } else {
+
+  }
+};
+
+const listaTitulos = [{nome: 'Mundial', ano: 1992, tipo: "internacional"}, {nome: 'Mundial', ano: 1993, tipo: "internacional"}]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Mudança feita N° 5!!!
-        </a>
+        <h2>Títulos do São Paulo</h2>
       </header>
+      <div className="quadroTabela">
+        <div className="quadroBotoes">
+          <button className="btn-busca" onClick={()=>busca('Todos')}>Todos os Títulos</button>
+          <button className="btn-busca" onClick={()=>busca('Estaduais')}>Títulos Estaduais</button>
+          <button className="btn-busca" onClick={()=>busca('Nacionais')}>Títulos Nacionais</button>
+          <button className="btn-busca" onClick={()=>busca('Internacionais')}>Títulos Internacionais</button>
+        </div>
+        <table className="tabela">
+          <tr>
+            <th>Título</th>
+            <th>Ano</th>
+          </tr>
+          {listaTitulos.map((titulo)=>(
+            <tr>
+            <td>{titulo.nome}</td>
+            <td>{titulo.ano}</td>
+          </tr>
+          ))}
+        </table>    
+      </div>
     </div>
   );
 }
